0284e1af923d601c1aef27adc8160893b5d41775
obadak2 <obadak2@gmail.com>
2025-07-02 12:57:57 +0000

    updated main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from controller.generator.RAGPipeline import RAGPipeline
 from .controller.Logger import setup_logger
+import json
 
 logger = setup_logger()
 logger.info("Starting application...")
 
 pipeline = RAGPipeline("example.pdf", logger)
 questions = pipeline.run("query")
-print(questions)
+with open('sample output.json', 'w', encoding='utf-8') as f:
+    json.dump(questions, f, ensure_ascii=False, indent=4)
